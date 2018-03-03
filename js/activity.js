d43b804263353b7b145e94742ca6c1e1731d04cd
Sebastian Silva <sebastian@fuentelibre.org>
2018-03-02 23:38:44 -0500

    Place serviceworker in root.

--- js/activity.js
+++ js/activity.js
@@ -32,7 +32,7 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('js/sw.js', {
+  navigator.serviceWorker.register('sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
