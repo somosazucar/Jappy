78b209c9cfb761231398d2704dc332533ed70734
Sebastian Silva <sebastian@fuentelibre.org>
2018-03-18 01:47:18 -0500

    Prevent failure on unavailable serviceworker

--- js/activity.js
+++ js/activity.js
@@ -28,13 +28,15 @@ define(["sugar-web/env", "sugar-web/activity/activity"], function (env, activity
 });
 
 //This is the "Offline copy of pages" service worker
-if (navigator.serviceWorker.controller) {
-  console.log('[PWA Builder] active service worker found, no need to register')
-} else {
-  //Register the ServiceWorker
-  navigator.serviceWorker.register('sw.js', {
-    scope: './'
-  }).then(function(reg) {
-    console.log('Service worker has been registered for scope:'+ reg.scope);
-  });
+if (navigator.serviceWorker) {
+      if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('sw.js', {
+      scope: './'
+    }).then(function(reg) {
+      console.log('Service worker has been registered for scope:'+ reg.scope);
+    });
+  }
 }
