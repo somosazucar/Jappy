0f7aff48529a570288fb27ffab783351323b352f
Sebastian Silva <sebastian@fuentelibre.org>
2018-04-11 00:27:43 -0500

    Remove cachebust

--- hooks.py
+++ hooks.py
@@ -1,11 +1,9 @@
 import subprocess
 from flask import jsonify
 from flask_hookserver import Hooks
-from flask_cache_bust import CacheBust
+
 
 def register_hooks(app):
-    cache_bust = CacheBust(app)
-
     app.config['VALIDATE_IP'] = False
     app.config['VALIDATE_SIGNATURE'] = False
 
@@ -16,17 +14,16 @@ def register_hooks(app):
     hooks.register_hook('ping', ping)
 
     def new_code(data, delivery):
-        print ('New push to %s' % data['ref'])
+        print('New push to %s' % data['ref'])
         if data['ref'] == 'refs/heads/master':
             try:
                 cmd_output = subprocess.check_output(
                     ['git', 'pull', 'origin', 'master'],)
                 return jsonify({'msg': str(cmd_output)})
             except subprocess.CalledProcessError as error:
-                print ("Code deployment failed", error.output)
+                print("Code deployment failed", error.output)
                 return jsonify({'msg': str(error.output)})
         return 'Thanks'
     hooks.register_hook('push', new_code)
 
     return None
-
