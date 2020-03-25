a0e96c78e280c7392ed58b9e9ae1b20776eb256f
Glitch (jappy) <none>
2020-03-25 07:30:02 +0000

    🎄💧 Checkpoint ./start.sh:1299563/42

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ set -eu
 # Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
 export PYTHONUNBUFFERED=true
 
+pip3 install --user -r requirements.txt
 
 PORT=8080 WORKSPACE='.data' python3 -m jappy.server
